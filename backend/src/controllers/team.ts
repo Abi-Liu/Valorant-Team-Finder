14b41127defef25a032833279970d99c3026687e
Abi Liu <abiliu018@gmail.com>
2023-06-29 23:47:18 -0700

    team creation functionality

--- backend/src/controllers/team.ts
+++ backend/src/controllers/team.ts
@@ -4,7 +4,12 @@ import Team from "src/models/Team";
 export default {
   create: async (req: Request, res: Response) => {
     try {
-      const { teamName } = req.body;
+      const { teamName, id } = req.body;
+      const team = await Team.create({ name: teamName });
+      const updatedTeam = await Team.findByIdAndUpdate(team._id, {
+        $push: { teammates: id },
+      });
+      res.status(200).json(updatedTeam);
     } catch (err) {
       console.error(err);
       res.status(500).json({ message: err });
