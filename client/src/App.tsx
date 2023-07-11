06ef4c666239dc4ee7a286edb85a081abe4fedd2
Abi-Liu <abiliu018@gmail.com>
2023-07-10 19:31:14 -0700

    added new fonts for typographys

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,34 +5,60 @@ import Home from "./pages/Home";
 import { useUserContext } from "./contexts/UserContext";
 import Navbar from "./components/Navbar";
 import { styled } from "@mui/material";
-import { CssBaseline } from "@mui/material";
+import { CssBaseline, createTheme } from "@mui/material";
 import Teams from "./pages/Teams";
+import ValorantFont from "./fonts/ValorantFont.ttf";
+import Tungsten from "./fonts/Tungsten.ttf";
+import { ThemeProvider } from "@emotion/react";
 
 const Offset = styled("div")(({ theme }) => theme.mixins.toolbar);
 
 function App() {
   const { loggedIn } = useUserContext();
-  console.log(loggedIn);
+
+  //used to create the custom Valorant font
+  const theme = createTheme({
+    typography: {
+      fontFamily: "Raleway, Arial, Poppins",
+    },
+    components: {
+      MuiCssBaseline: {
+        styleOverrides: `
+          @font-face {
+            font-family: 'Valorant';
+            font-style: bold;
+            font-display: swap;
+            font-weight: 400;
+            src: url(${ValorantFont}) format('ttf');
+            unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+          }
+        `,
+      },
+    },
+  });
+
   return (
     <>
       <CssBaseline />
-      <Navbar />
-      <Offset />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route
-          path="/login"
-          element={!loggedIn ? <Login /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/signup"
-          element={!loggedIn ? <Register /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/teams"
-          element={loggedIn ? <Teams /> : <Navigate to="/login" />}
-        />
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <Navbar />
+        <Offset />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/login"
+            element={!loggedIn ? <Login /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/signup"
+            element={!loggedIn ? <Register /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/teams"
+            element={loggedIn ? <Teams /> : <Navigate to="/login" />}
+          />
+        </Routes>
+      </ThemeProvider>
     </>
   );
 }
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,30 +1,8 @@
 import { Box, Typography, createTheme } from "@mui/material";
 import AgentImage from "../assets/Agents.png";
 import Background from "../assets/val-bg.jpg";
-import ValorantFont from "../fonts/ValorantFont.ttf";
-import { ThemeProvider } from "@emotion/react";
 
 const Home = () => {
-  //used to create the custom Valorant font
-  const theme = createTheme({
-    typography: {
-      fontFamily: "Raleway, Arial",
-    },
-    components: {
-      MuiCssBaseline: {
-        styleOverrides: `
-          @font-face {
-            font-family: 'Valorant';
-            font-style: normal;
-            font-display: swap;
-            font-weight: 400;
-            src: url(${ValorantFont}) format('ttf');
-            unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
-          }
-        `,
-      },
-    },
-  });
   return (
     <>
       <Box></Box>
@@ -48,24 +26,30 @@ const Home = () => {
         >
           <Box sx={{ pt: "12rem", width: "35%" }}>
             {/* consuming the theme provider to use the custom valorant font */}
-            <ThemeProvider theme={theme}>
-              <Typography
-                sx={{
-                  fontSize: "50px",
-                  textAlign: "center",
-                  color: "#ff3948",
-                  fontFamily: "Valorant",
-                }}
-                variant="h3"
-              >
-                Valorant
-              </Typography>
-            </ThemeProvider>
+
             <Typography
-              sx={{ color: "white", textAlign: "center" }}
+              sx={{
+                fontSize: "3rem",
+                textAlign: "center",
+                color: "#FC3C4D",
+                fontFamily: "Valorant",
+              }}
+              variant="h3"
+            >
+              VALORANT
+            </Typography>
+
+            <Typography
+              sx={{
+                color: "#EEE8E2",
+                textAlign: "center",
+                fontFamily: "Tungsten",
+                fontWeight: "bold",
+                fontSize: "7rem",
+              }}
               variant="h2"
             >
-              Team Finder
+              TEAM FINDER
             </Typography>
           </Box>
         </Box>
