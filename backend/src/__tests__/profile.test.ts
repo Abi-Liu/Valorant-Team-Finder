98ba366525325523a72aaa4dd0a441ca446fb464
Abi-Liu <abiliu018@gmail.com>
2023-07-13 00:07:57 -0700

    more tests

--- backend/src/__tests__/profile.test.ts
+++ backend/src/__tests__/profile.test.ts
@@ -2,7 +2,7 @@ import supertest from "supertest";
 import createServer from "../config/server";
 import mongoose from "mongoose";
 import { MongoMemoryServer } from "mongodb-memory-server";
-import { realUserInput } from "./utils/inputs";
+import { realUserInput, userRegisterInput } from "./utils/inputs";
 
 describe("Profile Routes", () => {
   //SET UP FOR TESTS
@@ -22,10 +22,13 @@ describe("Profile Routes", () => {
 
     //creates an authorized agent to test the protected routes
 
-    //agent used to test team creation based on all valid inputs
+    //agent used to test profile creation based on all valid inputs
     agent = supertest.agent(app);
+    //agent used to test profile creation based on authorized user, but invalid IGN
+    agent2 = supertest.agent(app);
 
     await agent.post("/auth/register").send(realUserInput);
+    await agent2.post("/auth/register").send(userRegisterInput);
   });
 
   afterAll(async () => {
@@ -35,12 +38,26 @@ describe("Profile Routes", () => {
   });
 
   //TESTS START HERE
+  //POST Profile route
   describe("create profile route", () => {
+    //given no user authorized
+    describe("given no user is authorized", () => {
+      it("should return a status 401 and a message of User not authorized", async () => {
+        //@ts-ignore
+        const { statusCode, body } = await supertest(app).post(
+          "/profile/createProfile"
+        );
+
+        expect(statusCode).toBe(401);
+        expect(body.message).toEqual("User not authorized");
+      });
+    });
+
+    //given a user is authorized with a real IGN
     describe("given user is authorized with a real ign", () => {
       it("should return a status of 200 and their profile data", async () => {
         //@ts-ignore
         const { statusCode, body } = await agent.post("/profile/createProfile");
-        console.log(body);
         expect(statusCode).toBe(200);
         expect(body).toEqual({
           __v: 0,
@@ -53,6 +70,19 @@ describe("Profile Routes", () => {
           region: expect.any(String),
           user: expect.any(String),
         });
+      });
+    });
+
+    //given a user is authorized with an invalid IGN
+    describe("given user is authorized with an invalid ign", () => {
+      it("should return a status of 400 and an error message", async () => {
+        //@ts-ignore
+        const { statusCode, body } = await agent2.post(
+          "/profile/createProfile"
+        );
+        console.log(body);
+        expect(statusCode).toBe(500);
+        expect(body.message).toEqual({});
       });
     });
   });
