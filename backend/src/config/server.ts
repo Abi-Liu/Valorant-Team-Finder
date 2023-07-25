898b0e502550082b896f8e6e1c2d2c1238e3083b
Abi-Liu <abiliu018@gmail.com>
2023-07-25 01:54:22 -0700

    reverted back to original

--- backend/src/config/server.ts
+++ backend/src/config/server.ts
@@ -23,22 +23,6 @@ import cron from "node-cron";
 export default function createServer() {
   const app = express();
   dotenv.config();
-
-  app.use(function (req, res, next) {
-    // res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Origin", req.headers.origin);
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
-    );
-    if ("OPTIONS" == req.method) {
-      res.send(200);
-    } else {
-      next();
-    }
-  });
-
   app.use(express.json());
   app.use(
     cors({ origin: "https://valorantfinder.netlify.app", credentials: true })
