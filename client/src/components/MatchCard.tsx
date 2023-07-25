a76e59eabae85b0f04ad2fd7ea4e3fa110d341e7
Abi-Liu <abiliu018@gmail.com>
2023-07-24 18:43:49 -0700

    added a drop down option to view match statistics on smaller screens

--- client/src/components/MatchCard.tsx
+++ client/src/components/MatchCard.tsx
@@ -1,6 +1,7 @@
-import { Avatar, Box, Grid, Typography } from "@mui/material";
-import { FC } from "react";
+import { Avatar, Box, Divider, Grid, Typography } from "@mui/material";
+import { FC, useState } from "react";
 import { Match } from "../interfaces/MatchResponse";
+import { ExpandMore } from "@mui/icons-material";
 
 interface MatchDataProps {
   matchData: Match;
@@ -9,6 +10,7 @@ interface MatchDataProps {
 }
 
 const MatchCard: FC<MatchDataProps> = ({ matchData, rank, rankImage }) => {
+  const [show, setShow] = useState(false);
   console.log(matchData);
   //headshot %
   const headshots = matchData.playerStats.headshots;
@@ -32,129 +34,217 @@ const MatchCard: FC<MatchDataProps> = ({ matchData, rank, rankImage }) => {
     lostRounds = matchData.redWon.rounds_lost;
   }
 
+  function showDetails() {
+    setShow((prev) => !prev);
+  }
+
   return (
     <Grid
       item
       xs={12}
       sx={{
         backgroundColor: "#1B2733",
-        display: "flex",
-        justifyContent: "space-around",
         my: "3px",
-        alignItems: "center",
         border: "1px solid #141414",
       }}
     >
-      <Box sx={{ py: "10px" }}>
-        <Avatar
-          src={matchData.image}
-          alt="Agent icon"
-          sx={{ width: "48px", height: "48px" }}
-        />
-      </Box>
-      <Box>
-        <Typography variant="body1" sx={{ color: "white" }}>
-          Competitive
-        </Typography>
-      </Box>
       <Box
         sx={{
+          width: "100%",
           display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
+          justifyContent: "space-around",
           alignItems: "center",
         }}
       >
-        <Avatar src={rankImage} alt="Rank Image" />
-        <Typography variant="caption" sx={{ color: "white" }}>
-          {rank}
-        </Typography>
-      </Box>
-      <Box sx={{ display: "flex", gap: 0.5, alignItems: "center" }}>
-        <Typography
-          variant="body1"
-          sx={{ fontSize: "1.15rem", color: "#32cd32" }}
-        >
-          {wonRounds}
-        </Typography>
-        <Typography
-          variant="body1"
+        <ExpandMore
+          sx={{ color: "white", display: { xs: "block", md: "none" } }}
+          onClick={showDetails}
+        />
+        <Box sx={{ py: "10px" }}>
+          <Avatar
+            src={matchData.image}
+            alt="Agent icon"
+            sx={{ width: "48px", height: "48px" }}
+          />
+        </Box>
+        <Box>
+          <Typography variant="body1" sx={{ color: "white" }}>
+            Competitive
+          </Typography>
+        </Box>
+        <Box
           sx={{
-            fontSize: "1.15rem",
-            color: "white",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
           }}
         >
-          :
-        </Typography>
-        <Typography
-          variant="body1"
-          sx={{ fontSize: "1.15rem", color: "#ED1C24" }}
-        >
-          {lostRounds}
-        </Typography>
-      </Box>
+          <Avatar src={rankImage} alt="Rank Image" />
+          <Typography variant="caption" sx={{ color: "white" }}>
+            {rank}
+          </Typography>
+        </Box>
+        <Box sx={{ display: "flex", gap: 0.5, alignItems: "center" }}>
+          <Typography
+            variant="body1"
+            sx={{ fontSize: "1.15rem", color: "#32cd32" }}
+          >
+            {wonRounds}
+          </Typography>
+          <Typography
+            variant="body1"
+            sx={{
+              fontSize: "1.15rem",
+              color: "white",
+            }}
+          >
+            :
+          </Typography>
+          <Typography
+            variant="body1"
+            sx={{ fontSize: "1.15rem", color: "#ED1C24" }}
+          >
+            {lostRounds}
+          </Typography>
+        </Box>
 
-      <Box
-        sx={{
-          display: { xs: "none", md: "flex" },
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "flex-end",
-        }}
-      >
-        <Typography variant="caption" sx={{ color: "#dddddd" }}>
-          K/D/A
-        </Typography>
-        <Typography variant="body1" sx={{ color: "white" }}>
-          {matchData.playerStats.kills} / {matchData.playerStats.deaths} /{" "}
-          {matchData.playerStats.assists}
-        </Typography>
+        <Box
+          sx={{
+            display: { xs: "none", md: "flex" },
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "flex-end",
+          }}
+        >
+          <Typography variant="caption" sx={{ color: "#dddddd" }}>
+            K/D/A
+          </Typography>
+          <Typography variant="body1" sx={{ color: "white" }}>
+            {matchData.playerStats.kills} / {matchData.playerStats.deaths} /{" "}
+            {matchData.playerStats.assists}
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: { xs: "none", md: "flex" },
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "flex-end",
+          }}
+        >
+          <Typography variant="caption" sx={{ color: "#dddddd" }}>
+            K/D
+          </Typography>
+          <Typography variant="body1" sx={{ color: "white" }}>
+            {kd}
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: { xs: "none", md: "flex" },
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "flex-end",
+          }}
+        >
+          <Typography variant="caption" sx={{ color: "#dddddd" }}>
+            HS%
+          </Typography>
+          <Typography variant="body1" sx={{ color: "white" }}>
+            {headshotPercent}
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: { xs: "none", md: "flex" },
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "flex-end",
+          }}
+        >
+          <Typography variant="caption" sx={{ color: "#dddddd" }}>
+            ADR
+          </Typography>
+          <Typography variant="body1" sx={{ color: "white" }}>
+            {Math.round(matchData.adr)}
+          </Typography>
+        </Box>
       </Box>
-      <Box
-        sx={{
-          display: { xs: "none", md: "flex" },
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "flex-end",
-        }}
-      >
-        <Typography variant="caption" sx={{ color: "#dddddd" }}>
-          K/D
-        </Typography>
-        <Typography variant="body1" sx={{ color: "white" }}>
-          {kd}
-        </Typography>
-      </Box>
-      <Box
-        sx={{
-          display: { xs: "none", md: "flex" },
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "flex-end",
-        }}
-      >
-        <Typography variant="caption" sx={{ color: "#dddddd" }}>
-          HS%
-        </Typography>
-        <Typography variant="body1" sx={{ color: "white" }}>
-          {headshotPercent}
-        </Typography>
-      </Box>
-      <Box
-        sx={{
-          display: { xs: "none", md: "flex" },
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "flex-end",
-        }}
-      >
-        <Typography variant="caption" sx={{ color: "#dddddd" }}>
-          ADR
-        </Typography>
-        <Typography variant="body1" sx={{ color: "white" }}>
-          {Math.round(matchData.adr)}
-        </Typography>
-      </Box>
+      {show && <Divider sx={{ bgcolor: "gray" }} />}
+      {show && (
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+            my: "1rem",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "flex-end",
+            }}
+          >
+            <Typography variant="caption" sx={{ color: "#dddddd" }}>
+              K/D/A
+            </Typography>
+            <Typography variant="body1" sx={{ color: "white" }}>
+              {matchData.playerStats.kills} / {matchData.playerStats.deaths} /{" "}
+              {matchData.playerStats.assists}
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "flex-end",
+            }}
+          >
+            <Typography variant="caption" sx={{ color: "#dddddd" }}>
+              K/D
+            </Typography>
+            <Typography variant="body1" sx={{ color: "white" }}>
+              {kd}
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "flex-end",
+            }}
+          >
+            <Typography variant="caption" sx={{ color: "#dddddd" }}>
+              HS%
+            </Typography>
+            <Typography variant="body1" sx={{ color: "white" }}>
+              {headshotPercent}
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "flex-end",
+            }}
+          >
+            <Typography variant="caption" sx={{ color: "#dddddd" }}>
+              ADR
+            </Typography>
+            <Typography variant="body1" sx={{ color: "white" }}>
+              {Math.round(matchData.adr)}
+            </Typography>
+          </Box>
+        </Box>
+      )}
     </Grid>
   );
 };
